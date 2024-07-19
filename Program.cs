8e29bd29b02ccff4910e151a168c6c054bd758bf
Priyanka08-dev <priyankalaxs@gmail.com>
2024-07-18 17:12:16 -1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your name");
+            string myname = Console.ReadLine();
+            Console.WriteLine(myname);
+            Console.WriteLine("Enter your Age");
+            int myage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(myage);
+            string result = (myage >= 20) ? "eligible to vote" : "Not";
+            Console.WriteLine(result);
+            if (myage >= 18)
+            {
+                Console.WriteLine("Eligible to vote");
+            }
+            else {
+                Console.Write("not eligible");
+            }
+        }
+    }
+}
